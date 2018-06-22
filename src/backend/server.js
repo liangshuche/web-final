3b5aaa0aa04ea8afb0882c3275cd6253cb70e60e
Yasheed1995 <qwerty8608301@gmail.com>
2018-06-22 11:37:04 +0800

    modified server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,24 +3,83 @@ const socket = require('socket.io');
 const mongoose = require('mongoose');
 const app = express();
 
+// require bady-parser for parsing from data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:a12345@ds159100.mlab.com:59100/web-blog');
+//mongoose.connect('mongodb://admin:a12345@ds159100.mlab.com:59100/web-blog');
+mongoose.connect('mongodb://localhost/web_test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback) {
+    console.log('connection open')
+});
 
 const postsSchema = mongoose.Schema({
-	user: String,
+	  user: String,
   	title: String,
 	  content: String,
 	  time: String,
 });
 
 const Posts = mongoose.model('Posts', postsSchema);
+// test object
+var TestPost = new Posts({
+    user: "Kamehameha",
+    title: "Havana",
+    content: "Do it now Gohan!"
+    time: '1997/08/31'
+})
+
+console.log(TestPost);
+
+app.get('/', function(req, res){
+    res.redirect('/todos')
+})
+
+app.get('/todos', function(req, res){
+  Posts.find(function(err, posts) {
+    if(err) return console.error(err);
+    console.log("existing posts are: ", posts);
+    // render index page, passing posts as local variable
+    res.render('index', {
+      posts: posts
+    })
+  })
+});
+
+app.post('/todos', function(req, res) {
+  var post = new Posts({
+    user: req.body.user,
+    title: req.body.title,
+    content: req.body.content,
+    time: req.body.time
+  })
+  console.log("created post: ", post);
+  // save post in db!
+  post.save(function(err, todo) {
+    if(err) return console.error(err);
+    console.log('save successful')
+    return post;
+  })
+  // return json of post
+  res.json(post);
+})
+
+//
 
 const server = app.listen(5000, () => {
   	console.log('server is running on port 5000');
 });
-
+/*
 const io = socket(server);
 
+*/
+/*
 const userList = [
 	  { username: 'admin', password: '123' , age: '20'},
 	  { username: 'tony' , password: '123', age: '21'}
@@ -64,8 +123,8 @@ io.on('connection', (socket) => {
 		const newPosts = new Posts({
 	  		user: data.user,
 	  		title: data.title,
-			content: data.content,
-			time: data.time,  
+			  content: data.content,
+			  time: data.time,  
 		});
 
 		newPosts.save().then(() => {
@@ -112,4 +171,6 @@ io.on('connection', (socket) => {
 		}).catch((err) => {console.log(err)});
 	});
 });
+*/
+
 
